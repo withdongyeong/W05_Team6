719c9dfc37cad4b89e03b558bcdc88c9107ecae4
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-03 22:59:48 +0900

    [Feat] 씬 Transform 재배치 & 요원 프리팹화 & 버튼 생성

---
+++ Assets/Private/JH/Scripts/PilotButton.cs
@@ -0,0 +1,27 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class PilotButton : MonoBehaviour
+{
+    public string actionId;
+    
+    private Player _player;
+    private Pilot _pilot;
+    private PilotActionDataList _pilotActions;
+
+
+    private void Awake()
+    {
+        _player = GetComponentInParent<Player>();
+        _pilot = GetComponentInParent<Pilot>();
+        _pilotActions = DataLoader.LoadPilotActions();
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Button Clicked");
+        var matchedAction = _pilotActions.pilotActions.Find(a =>
+        a.pilot == _pilot.pilotId && a.id == actionId);
+        _player.IssueCommand(_pilot, matchedAction);
+    }
+}
