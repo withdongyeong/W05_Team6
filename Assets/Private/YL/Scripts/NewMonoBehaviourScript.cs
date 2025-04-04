82638b8d90f266f73e3546ecbbb54733324f4fd2
je-ya <psychicartil@naver.com>
2025-04-04 13:17:25 +0900

    비주얼 스크립팅 테스트 & Dotween, cinemachine 설치

---
+++ Assets/Private/YL/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    //인코딩 테스트
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
