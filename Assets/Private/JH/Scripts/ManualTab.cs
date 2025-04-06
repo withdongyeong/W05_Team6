0fa5b814138ce8c80f62edd00a56caac76a7f65f
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-06 16:59:08 +0900

    [Refactoring] 코드수정

--- Assets/Private/JH/Scripts/ManualTab.cs
+++ Assets/Private/JH/Scripts/ManualTab.cs
@@ -2,23 +2,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-//public class ManualTab : MonoBehaviour, IPointerClickHandler
-//{
-//    public int ManualIndex;
-
-//    public void OnPointerClick(PointerEventData eventData)
-//    {
-//        Debug.Log("Tab Clicked");
-//        if (ManualIndex == 0)
-//        {
-//            UIManager.Instance.SetAttackManualScreen();
-//        }
-//        if (ManualIndex == 1)
-//        {
-//            UIManager.Instance.SetDefenseManualScreen();
-//        }
-//    }
-//}
 public class ManualTab : MonoBehaviour
 {
     public int ManualIndex;
