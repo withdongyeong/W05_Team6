fe03553f26f4526cb55314621366d2ada34a44be
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-06 21:49:26 +0900

    [Feat] 선택 시 같은 쌍끼리 테두리 강조

--- Assets/Private/JH/Scripts/PilotButton.cs
+++ Assets/Private/JH/Scripts/PilotButton.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections.Generic;
 using Unity.VisualScripting;
+using SpriteGlow;
 using UnityEngine;
 
 public class PilotButton : MonoBehaviour
 {
     public int pilotId;
+    public SpriteGlowEffect PairingObjectGlow;
 
     private PilotActionButton[] _pilotActionButtons;
+    private SpriteGlowEffect _objectGlow;
     private Collider2D _collider;
 
+    private void Awake()
+    {
+        _objectGlow = GetComponent<SpriteGlowEffect>();
+    }
 
     private void Start()
     {
@@ -32,16 +39,24 @@ public class PilotButton : MonoBehaviour
 
     private void Open()
     {
+        _objectGlow.OutlineWidth = 10;
+        PairingObjectGlow.OutlineWidth = 10;
+
         for (int i = 0; i < _pilotActionButtons.Length; i++)
         {
             _pilotActionButtons[i].gameObject.SetActive(true);
         }
         UIManager.Instance.OnDisableUI?.Invoke();
     }
-
+     
     private void Close()
     {
+        //_objectGlow = GetComponent<SpriteGlowEffect>();
+        _objectGlow.OutlineWidth = 0;
+        //PairingObjectGlow.OutlineWidth = 0;
+
         _collider.enabled = true;
+
         for (int i = 0; i < _pilotActionButtons.Length; i++)
         {
             _pilotActionButtons[i].gameObject.SetActive(false);
---
+++ Assets/Private/JH/Scripts/UI_GameEnd.cs
@@ -0,0 +1,23 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_GameEnd : MonoBehaviour
+{
+    public Sprite GameClearSprite;
+    public Sprite GameOverSprite;
+    private Canvas _canvas;
+    private Image _image;
+
+    private void Awake()
+    {
+        _canvas = GetComponentInParent<Canvas>();
+        _image = GetComponent<Image>();
+    }
+
+    public void ShowGameEndUI(bool isClear)
+    {
+        _canvas.enabled = true;
+        _image.sprite = isClear ? GameClearSprite : GameOverSprite;
+    }
+}
