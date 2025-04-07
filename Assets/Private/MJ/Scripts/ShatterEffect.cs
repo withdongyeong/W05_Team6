ba09bd27670e9f0698ff863ac962c178838ce5f2
scopedogtc <rhdrks1440@gmail.com>
2025-04-07 17:26:56 +0900

    0407/1726

---
+++ Assets/Private/MJ/Scripts/ShatterEffect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShatterEffect : MonoBehaviour
+{
+    RawImage rawimage;
+    private void Awake()
+    {
+        rawimage = GetComponent<RawImage>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameManager.Instance.Player.ShatterAction += StartShatterEffect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ShatterEffectCoroutine()
+    {
+        Color color = rawimage.color;
+        color.a = 1f;
+        rawimage.color = color;
+        yield return new WaitForSeconds(GlobalSettings.Instance.ShatterEffectDuration);
+        float currentTime = 0f;
+        while(currentTime < 2.5f)
+        {
+            color.a = Mathf.Lerp(1, 0, currentTime / 2.5f);
+            rawimage.color = color;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+    }
+
+    private void StartShatterEffect()
+    {
+        StartCoroutine("ShatterEffectCoroutine");
+    }
+}
